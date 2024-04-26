74bd7769efaf6f1f7d89db6d18c21d8049d499ca
Jan Severo <jansevero@gmail.com>
2024-04-26 02:57:13 -0300

    crud alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'Jana',
-      sobrenome: 'Severo',
-      email: 'jana@gmail.com',
-      idade: 23,
-      peso: 50,
-      altura: 1.54,
-    });
-    res.json(novoAluno);
+    res.json('ok');
   }
 }
 
